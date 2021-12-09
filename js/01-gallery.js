eedd1070247e48d8693d19353bb6ea65febb0239
IhorKozloff <ik.eyeshield21@gmail.com>
2021-12-09 16:01:17 +0200

    begin

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+// 1 Создание Галереи
+function createGalleryItemMarkUp (cardsInform) {
+   return cardsInform.map(item => {
+            return `<div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+                class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="${item.description}"
+                />
+            </a>
+        </div>`
+    }).join('');
+};
 
-console.log(galleryItems);
+galleryEl.innerHTML = createGalleryItemMarkUp(galleryItems);
+
+
+function noFollowByImgLinkClick (e) {
+    e.preventDefault();
+};
+
+function getOriginalImgUrl (event) {
+    if (event.target.nodeName != `IMG`) {
+        return
+    }
+    console.log(event.target.dataset.source);
+};
+
+galleryEl.addEventListener('click', noFollowByImgLinkClick);
+galleryEl.addEventListener('click', getOriginalImgUrl);
