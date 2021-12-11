f3a7472851613da2ce0dc5439db3de960e6cf784
IhorKozloff <ik.eyeshield21@gmail.com>
2021-12-11 11:27:43 +0200

    homework 7 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,35 +1,62 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryEl = document.querySelector('.gallery');
-// 1 Создание Галереи
-function createGalleryItemMarkUp (cardsInform) {
-   return cardsInform.map(item => {
-            return `<div class="gallery__item">
-            <a class="gallery__link" href="${item.original}">
-                <img
-                class="gallery__image"
-                src="${item.preview}"
-                data-source="${item.original}"
-                alt="${item.description}"
-                />
-            </a>
-        </div>`
-    }).join('');
-};
 
+const instance = basicLightbox.create(`
+<img src="" width="800" height="600">
+`);
+
+const image = instance.element().querySelector('img');
+
+function createGalleryItemMarkUp (cardsInform) {
+    return cardsInform.map(item => {
+             return `<div class="gallery__item">
+             <a class="gallery__link" href="${item.original}">
+                 <img
+                 class="gallery__image"
+                 src="${item.preview}"
+                 data-source="${item.original}"
+                 alt="${item.description}"
+                 />
+             </a>
+         </div>`
+     }).join('');
+ };
 galleryEl.innerHTML = createGalleryItemMarkUp(galleryItems);
 
+function onClickImgElement (event) {
+    event.preventDefault();
 
-function noFollowByImgLinkClick (e) {
-    e.preventDefault();
-};
-
-function getOriginalImgUrl (event) {
     if (event.target.nodeName != `IMG`) {
         return
     }
-    console.log(event.target.dataset.source);
+    const originalImgUrl = event.target.dataset.source;
+
+
+    image.src = `${originalImgUrl}`; 
+    instance.show();
+
+    document.addEventListener('keydown', closeEscBtn);
+
+    console.log(originalImgUrl);
+    return originalImgUrl; 
+
 };
 
-galleryEl.addEventListener('click', noFollowByImgLinkClick);
-galleryEl.addEventListener('click', getOriginalImgUrl);
+function closeEscBtn (event) {
+    if (event.key === 'Escape') {
+       instance.close();
+       document.removeEventListener('keydown', closeEscBtn);
+    }
+};
+
+galleryEl.addEventListener('click', onClickImgElement);
+
+
+ 
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,68 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+
+
+function createGalleryItemMarkUp (cardsInform) {
+
+return cardsInform.map(item => {
+    return `
+    <a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.original}" alt="${item.description}" />
+    </a>
+    `
+}).join('');
+}; 
+
+galleryEl.innerHTML = `${createGalleryItemMarkUp(galleryItems)}`;
+
+
+
+function onClickImgElement (event) {
+    event.preventDefault()
+};
+
+const galleryFitches = new SimpleLightbox('.gallery a', {});
+
+function findBySrc (findedObject, findedItem) {
+    const descriptionValue = findedObject.find(item => item.original === findedItem).description;
+ return descriptionValue;
+ };
+
+function addPictureName (className, currentImg) {
+    const imageWrapper = document.querySelector(`.${className}`);
+    const srcImage = document.querySelector(`.${currentImg}`).src;
+    imageWrapper.insertAdjacentHTML('beforeend', `<div class="active-img"> <p style="opacity: 1"> ${findBySrc(galleryItems, srcImage)} </p> </div>`); 
+};
+
+function removePictureName (activeImg) {
+    const labelEl = document.querySelector(`.${activeImg}`)
+    if (labelEl) {
+        labelEl.remove();
+    }
+};
+
+
+// Срабатывает при открытии
+galleryFitches.on('shown.simplelightbox', _.debounce(function () {
+    addPictureName('sl-image', 'sl-image img');
+}, 250));
+// Срабатывает пред тем как картинка изменится
+galleryFitches.on('change.simplelightbox', function () {
+    removePictureName ('active-img');
+});
+
+//Срабатывает после того как картинка изменится
+galleryFitches.on('changed.simplelightbox', _.debounce(function () {
+    addPictureName('sl-image', 'sl-image img');
+}, 250));
+
+// Срабатывает пред закрытием
+galleryFitches.on('close.simplelightbox', function () {
+    removePictureName ('active-img');
+});
+
+galleryEl.addEventListener('click', onClickImgElement);
